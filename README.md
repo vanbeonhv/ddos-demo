7bcf3480256e6a33e3b6a4ac51a7c576eeec90ac
marc-ubuntu-pc <nguyenvannhv26@gmail.com>
2025-04-07 16:51:06 +0700

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+dos-demo/
+│
+├── docker-compose.yml
+│
+├── defender/
+│   ├── Dockerfile
+│   ├── nginx.conf
+│   └── server.js
+│
+└── attacker/
+    └── Dockerfile
---
+++ node/server.js
@@ -0,0 +1,5 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.end('Hello from Node.js \n');
+});
+server.listen(3000, () => console.log('Server running on port 3000'));
